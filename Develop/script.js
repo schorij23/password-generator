61d971d34e1b36d120d01abea587e8cd45efee16
schorij23 <schorij23@gmail.com>
2023-09-04 20:21:51 -0400

    third commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment code here
-//would start like var writePassword()
+// would start like var writePassword()
+// var generateBtn = document.querySelector("#generate");
+// function generatePassword() {
+// // Critia prompts for password to generate
+// var uppercase = confirm("Your password should have an uppercase letter! Click OK to continue");
+// var lowercase = confirm("Your password should have a lowercase letter! Click OK to continue");
+// var symbols = confirm("Your password should have a symbol! Click OK to continue");
+// var numbers = confirm("Your password should have a number! Click OK to continue");
+// var keyLength = prompt("Password must be between 8 and 128 characters! Click OK to continue");
 
+var writePassword
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
