d5fe5dd1de7b220e655112cea2e7bf17ab4e0dd6
schorij23 <schorij23@gmail.com>
2023-09-06 21:07:16 -0400

    all special chars

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseChar = ['A', 'B', 'C', 'D', 'E', "F", 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ':', '.', '-', '_', '/',];
+var specialChar = [' ','!','"','#','$','%','&','(',')','*','+',',','-','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~'];
 
 //function for password prompt and variable to store password length
 function passwordCharOptions() {
